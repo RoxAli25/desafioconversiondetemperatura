a8164caecc08cca654130c0e381f98fdc5ff42d4
Roxana Aliaga <andrea.200210@gmail.com>
2025-04-22 13:54:24 -0400

    Actualizando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Desafío conversión de Temperatura");
+
+        double temperaturaEnCelsius = 15.6;
+        double temperaturaEnFahrenheit = (temperaturaEnCelsius * 1.8) + 32;
+
+        String mensaje = String.format ("La temperatura de %f Celsius es equivalente a %f Fahrenheit", temperaturaEnCelsius, temperaturaEnFahrenheit);
+
+        System.out.println(mensaje);
+
+        int gradosFahrenheit = (int) temperaturaEnFahrenheit;
+
+        System.out.println("La temperatura en Fahrenheit es:" + gradosFahrenheit);
     }
 }
